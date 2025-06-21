ce1f97435fb8c439eaefc02fc21b9136ea4fdcb1
gensichen <gensichen@gmail.com>
2025-06-21 14:20:10 +0200

    Add boilerplate spring boot project
    We will build from here and learn as we go.

---
+++ bank-withdrawal-api/src/main/java/com/bank/bankwithdrawalapi/BankWithdrawalApiApplication.java
@@ -0,0 +1,13 @@
+package com.bank.bankwithdrawalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankWithdrawalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankWithdrawalApiApplication.class, args);
+    }
+
+}
---
+++ bank-withdrawal-api/src/test/java/com/bank/bankwithdrawalapi/BankWithdrawalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.bankwithdrawalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankWithdrawalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
