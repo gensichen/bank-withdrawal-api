72d42e4736bcc78b0764f9cba1a4fd0055f5d4c0
gensichen <gensichen@gmail.com>
2025-06-22 10:57:50 +0200

    Add missing libary import.

--- bank-withdrawal-api/src/main/java/com/bank/bankwithdrawalapi/controller/BankAccountController.java
+++ bank-withdrawal-api/src/main/java/com/bank/bankwithdrawalapi/controller/BankAccountController.java
@@ -6,7 +6,7 @@ import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.PublishRequest;
 import software.amazon.awssdk.services.sns.model.PublishResponse;
 import java.math.BigDecimal;
-import org.springframework.beans.factory.annotation.Autowired; //
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @RestController
